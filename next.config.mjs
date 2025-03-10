945505415214f465d7c319b099d97db132700612
mangugeng <mangugeng@yahoo.com>
2025-03-11 03:41:26 +0700

    Update Next.js config to ignore ESLint during builds

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    transpilePackages: ['three'],
+    webpack: (config) => {
+        config.externals.push({
+            'utf-8-validate': 'commonjs utf-8-validate',
+            'bufferutil': 'commonjs bufferutil',
+        })
+        return config
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'accelerometer=*, gyroscope=*, magnetometer=*, xr-spatial-tracking=*'
+                    }
+                ],
+            },
+        ]
+    },
+    output: 'standalone',
+    eslint: {
+        ignoreDuringBuilds: true
+    }
+}
+
+export default nextConfig 
