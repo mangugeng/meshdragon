e04ca8016bee52717a91b426b219129b1d679e20
mangugeng <mangugeng@yahoo.com>
2025-03-11 03:36:05 +0700

    Add ESLint config and fix TypeScript errors

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -15,7 +15,7 @@ export default async function LocaleLayout({
   let messages
   try {
     messages = (await import(`../../messages/${locale}.json`)).default
-  } catch {
+  } catch (_error) {
     notFound()
   }
 
